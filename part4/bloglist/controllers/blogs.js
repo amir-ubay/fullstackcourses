6d15b87f1a134d51b5b3cec6161fde8ba9005352
HP Ubaidillah Amir <amir.ubaidillah@gmail.com>
2023-11-17 08:59:48 +0700

    update env

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,31 +1,60 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', (request, response, next) => {
   Blog
     .find({})
+    .populate('user', { username: 1, name: 1 })
     .then(blogs => { 
       response.json(blogs)
     })
   .catch(error => next(error))
 })
 
-blogsRouter.post('/', (request, response, next) => {
-  const body = request.body
-  const blog = new Blog(body)
+// blogsRouter.post('/', (request, response, next) => {
+//   const body = request.body
+//   const blog = new Blog(body)
 
-  if (!Object.prototype.hasOwnProperty.call(body, "likes")) {
-    blog.likes = 0
-  } else if (body.title === undefined || body.url === undefined) {
-    return response.status(400).json({ error: 'title or url missing' })
+//   if (!Object.prototype.hasOwnProperty.call(body, "likes")) {
+//     blog.likes = 0
+//   } else if (body.title === undefined || body.url === undefined) {
+//     return response.status(400).json({ error: 'title or url missing' })
+//   }
+
+//   blog
+//       .save()
+//       .then(result => {
+//         response.status(200).json(result)
+//       })
+//       .catch(error => next(error))
+// })
+
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
+
+  // Check if the username exist
+  const existingUser = await User.findOne({ username: body.username })
+  if (!existingUser) {
+    return response.status(400).json({error: 'No user found'})
   }
 
-  blog
-      .save()
-      .then(result => {
-        response.status(200).json(result)
-      })
-      .catch(error => next(error))
+  // Create a new blog with user data
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: existingUser._id
+  })
+
+  const savedBlog = await blog.save()
+
+  existingUser.blogs = existingUser.blogs.concat(savedBlog)
+
+  await existingUser.save()
+
+  response.status(200).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', (request, response, next) => {
--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt')
 usersRouter.get('/', (request, reponse, next) => { 
   User
     .find({})
+    .populate('blogs', {url: 1, title: 1, author: 1, like: 1 })
     .then(users => {
       reponse.json(users)
     })
@@ -13,6 +14,17 @@ usersRouter.get('/', (request, reponse, next) => {
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
+  
+  // Check if username and password are provided and have at least 3 characters
+  if (!username || !password || username.length < 3 || password.length < 3) {
+    return response.status(400).json({ error: 'Username and password must be provided and have at least 3 characters' })
+  }
+
+  // Check if the username is unique
+  const existingUser = await User.findOne({ username })
+  if (existingUser) {
+    return response.status(400).json({ error: 'Username already exists' })
+  }
   
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
